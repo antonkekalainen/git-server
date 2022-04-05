56a5d1b9f4e51d4ca061b6c7d731cec833d3382c
Tanko <tanko@zfc.moe>
2022-04-05 17:15:27 +0300

    Clean up pg_init.sql

--- pg_init.sql
+++ pg_init.sql
@@ -1,6 +1,5 @@
 CREATE TABLE objects (hash BYTEA PRIMARY KEY, pack TEXT NOT NULL, type TEXT NOT NULL, compressed_size INTEGER NOT NULL, size INTEGER NOT NULL, location INTEGER NOT NULL);
 CREATE TABLE deltas (hash BYTEA PRIMARY KEY, pack TEXT NOT NULL, base BYTEA NOT NULL, compressed_size INTEGER NOT NULL, size INTEGER NOT NULL, location INTEGER NOT NULL);
 CREATE TABLE parents (child BYTEA PRIMARY KEY, parents BYTEA ARRAY NOT NULL);
--- CREATE INDEX child_idx ON parents (child);
 CREATE INDEX ON parents USING hash (child);
 CREATE TABLE refs (repo TEXT NOT NULL, name TEXT NOT NULL, hash BYTEA NOT NULL);
